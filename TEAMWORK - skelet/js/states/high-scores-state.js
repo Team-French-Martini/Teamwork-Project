6c80de6eea29418b9f6ba1aebedecbf6ced105c9
martintabakov <martin.tabakov.e@gmail.com>
2016-08-11 10:55:54 +0300

    Create high-scores-state.js

---
+++ TEAMWORK - skelet/js/states/high-scores-state.js
@@ -0,0 +1,38 @@
+function HighScoresState(gameStateManagerObj) {
+    this.layer = new Kinetic.Layer();
+    this.gameStateManager = gameStateManagerObj,
+    this.background = new HighScoresBackground(),
+    this.render = function () {
+        this.layer.batchDraw();
+    },
+    this.update = function () {
+      
+    },
+
+    this.highScoresText = new Kinetic.Text({
+        x: 300,
+        y: 100,
+        fontSize: 30,
+        fontFamily: 'Calibri',
+        fill: 'blue'
+    });
+
+    var highScoresString = "";
+    var i = 1;
+    for (var key in localStorage) {
+        highScoresString += i + ". " + formatDate(new Date(key)) + " " + localStorage[key] + "\n";
+        i++;
+    }
+
+    this.highScoresCloseButton = new HighScoresCloseButton();
+
+    this.highScoresText.setText(highScoresString);
+
+    attachHighScoresStateButtonsEvents(this);
+    this.layer.add(this.background.backgroundImage);
+    this.layer.add(this.highScoresCloseButton.buttonImage);
+    this.layer.add(this.highScoresText);
+
+
+    this.gameStateManager.stage.add(this.layer);
+}
